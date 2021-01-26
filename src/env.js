d9da4e80d71d6e4be4a128b987c850f53b71d309
ahmad ardiansyah <ahmad.ardiansyah@carsworld.co.id>
2021-01-26 18:05:18 +0700

    change env

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const DOCKER_URL     = "https://docker.projekku.carsworld.co.id";
 export const KONG_URL       = "https://kong.projekku.carsworld.co.id";
-// export const API_URL        = "https://api.projekku.carsworld.co.id";
-export const API_URL        = "http://localhost:3000";
+export const API_URL        = "https://api.projekku.carsworld.co.id";
+// export const API_URL        = "http://localhost:3000";
 export const XMYSQL_URL     = "https://xmysql.projekku.carsworld.co.id";
